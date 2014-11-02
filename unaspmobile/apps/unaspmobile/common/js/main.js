05d6a582fb1d378c94d2354a1a271436a433964e
padilha <padilha@padilha-PC>
2014-11-02 05:44:14 -0200

    dadsf

--- unaspmobile/apps/unaspmobile/common/js/main.js
+++ unaspmobile/apps/unaspmobile/common/js/main.js
@@ -9,64 +9,19 @@ var CURSO_NOME ="";
 
 //função inicial
 function wlCommonInit(){
-
 	verificaLogin();
 	resposive();
-	loadAplication("Carregando...");
+	loadAplication("Carregando..");
 	//ajaxLoader('#mostra_detalhes_curso');
 
 	if (WL.Client.getEnvironment() == WL.Environment.WINDOWS_PHONE_8) {
 		path = "www/default/";
 	}
-
-}
-
-function funcoesNecesarias(){
-	verificaDadoExistente(USERSESSION.NOME,"#display-name-user");
-	verificaDadoExistente(USERSESSION.EMAIL,"#display-email-user");
-	loadPerfil(USERSESSION.RA);
-	loadFinanceiro(USERSESSION.RA,'all','');
-	loadFinanceiro(USERSESSION.RA,'option');
-	$.mobile.changePage("#perfil");
-	$("#header-menu").show();
-	active_menu('.botoes-menu li:eq(2)');
-	$('#footer-fixed').show();
-	$('.bar').show();
 }
 
 $('.botoes-menu li').click(function(){
 	active_menu(this);
 });
-
-
-function carregar(texto){
-	$('#form').hide();
-	$('#footer-fixed').hide();
-	$('.bar').hide();
-	$(".loadMobile").fadeIn(3000);
-	$(".loadMobile h3").text(texto);
-	function mostradf(){
-		$(".loadMobile h3").fadeIn("slow");
-		$(".loadMobile h3").fadeOut("slow");
-	}
-	setInterval(function(){mostradf()},2000);
-}
-
-function carregado(){
-	$('#form').show('slow');
-	$('#footer-fixed').show();
-	$('.bar').show();
-	$(".loadMobile").hide('slow');
-}
-
-function loadAplication(texto){
-	texto
-	carregar(texto);
-	setTimeout(function(){
-		carregado();	
-	},8000);
-}
-
 
 
 
